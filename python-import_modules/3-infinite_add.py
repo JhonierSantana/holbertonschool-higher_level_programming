cf4be391faf13557b7d0bd6b2df659cd18893350
root <root@DESKTOP-ISQ43BP>
2023-01-27 14:54:36 -0500

    ~3-infinite_add.py

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    sum = 0
+    i = 1
+    argc = len(sys.argv)
+    while (i < argc):
+        sum += int(sys.argv)
+        i += 1
+    print("{:d".format(sum))
