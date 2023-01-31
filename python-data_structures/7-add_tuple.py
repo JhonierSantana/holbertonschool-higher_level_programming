1903e8d77c3d43726e73ff25527cde5594e1fa62
root <root@DESKTOP-ISQ43BP>
2023-01-31 13:33:44 -0500

    7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b()):
+def add_tuple(tuple_a=(), tuple_b=()):
     if (len(tuple_a) >= 2):
         addend_1 = tuple_a
     if (len(tuple_b) >= 2):
