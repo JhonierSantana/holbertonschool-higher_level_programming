d00b01bb8a17d1aea893e61a65bd5072d0cbb11e
root <root@DESKTOP-ISQ43BP>
2023-02-07 11:01:27 -0500

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Firs class with exeptions"""
+
+
 class Square:
+    """Class with exeptions"""
     def __init__(self, size=0):
         if (isinstance(size, int) is False):
             raise TypeError("size must be an integer")
