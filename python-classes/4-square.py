ed1185f7cf2d928c57744d1adf0e4e9edbbe48f7
root <root@DESKTOP-ISQ43BP>
2023-02-07 14:32:56 -0500

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Class with getter and setter"""
+""" Class with getter and setter"""
 
 
 class Square:
-    """Init for instantiate"""
+    """ Init method for instantiate """
     def __init__(self, size=0):
         self.size = size
-    
+
     @property
     def size(self):
-        """Size"""
+        """ Size"""
         return self._size
-    
+
     @size.setter
     def size(self, value):
         if (isinstance(value, int) is False):
@@ -20,8 +20,8 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self._size = value
-            
-        """Squeare Area"""
-        def area(self):
-            area = self._Square__size ** 2
-            return area
+
+    """ Square Area """
+    def area(self):
+        area = self.size ** 2
+        return area
