c05cfa1c0b8de91402d9fe75cdad1adbab32e939
root <root@DESKTOP-ISQ43BP>
2023-02-07 10:23:05 -0500

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""First class with atributte"""
+
+
+class Square:
+    """Class with atributte"""
+    def __init__(self, size):
+        self._Square__size = size
+        pass
