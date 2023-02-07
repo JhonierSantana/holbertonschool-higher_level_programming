34f828685844f25a752cb43db7e50b9b35d98c7d
root <root@DESKTOP-ISQ43BP>
2023-02-07 13:34:50 -0500

    3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""CLass calculating the square area"""
+
+
+class Square:
+    """Class with size and area"""
+    def __init__(self, size=0):
+        if (isinstance(size, int) is False):
+            raise IndexError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    """Square Area"""
+    
+         
+    def area(self):
+        area = self._Square__size ** 2
+        return area
