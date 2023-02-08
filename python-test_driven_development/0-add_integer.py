cc2c114c75e1079b7d74b80eca8fec0930ef0e2e
root <root@DESKTOP-ISQ43BP>
2023-02-08 12:04:52 -0500

    test

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Add integer"""
+def add_integer(a, b=98):
+    """Arguments:
+    a: First number to add (int or float)
+    b: Second number to add (int or float)
+    
+    Returns:
+        int: The sum of both numbers casted to integers.
+        TypeErorr if a or b is not a number
+    """
+    
+    if isinstance(a, int) is False and isinstance(a, float) is False:
+        raise TypeError("a must be an integer")
+    if isinstance(b, int) is False and isinstance(b, float) is False:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
