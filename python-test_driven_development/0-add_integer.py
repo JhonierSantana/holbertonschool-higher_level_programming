ff78bb768ca195dceae8e1a4bd437e12234e2878
root <root@DESKTOP-ISQ43BP>
2023-02-08 14:11:38 -0500

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""Add integer"""
+""" Add integer
+    This modlue adds two numbers """
+
+
 def add_integer(a, b=98):
-    """Arguments
+    """ Arguments:
         a: First number to add (int or float)
         b: Second number to add (int or float)
-        
     Returns:
-        int: The sum of both numbers casted to integers
+        int: The sum of both numbers casted to intigers.
         TypeError if a or b is not a number
     """
-    
+
     if isinstance(a, int) is False and isinstance(a, float) is False:
         raise TypeError("a must be an integer")
     if isinstance(b, int) is False and isinstance(b, float) is False:
