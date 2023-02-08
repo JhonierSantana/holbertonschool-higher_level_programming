ca45d8f208e1acc21a07bbaa9e0f48557a2c4948
root <root@DESKTOP-ISQ43BP>
2023-02-08 14:07:22 -0500

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -15,7 +15,3 @@ def add_integer(a, b=98):
     if isinstance(b, int) is False and isinstance(b, float) is False:
         raise TypeError("b must be an integer")
     return int(a) + int(b)
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
