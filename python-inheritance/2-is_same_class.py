b40097ddfca377c26adcea05c3ebc2cea646eadd
root <root@DESKTOP-ISQ43BP>
2023-02-14 10:31:54 -0500

    2-is_same_class.py

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Module to check if an object is exactly a class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+        is_same_class - Return true if obj is exactly an
+        instance of a_class
+    """
+    return (type(obj) is a_class)
