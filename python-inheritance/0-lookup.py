40bae59f821b7cb19a3acc303f5fa4fdc37de2b1
root <root@DESKTOP-ISQ43BP>
2023-02-14 09:39:05 -0500

    0-lookup.py

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Modue to return a list"""
+
+def lookup(obj):
+    """
+    lookup - Return a list with object
+    Argu:
+        obj - Object to bild the list
+        
+    Return: 
+        list
+    """
+    return dir(obj)
