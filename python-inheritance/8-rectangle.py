bbc7bf83d778365be979a8cb6dc5f63425b53afa
root <root@DESKTOP-ISQ43BP>
2023-02-15 09:12:49 -0500

    8-rectangle.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -24,6 +24,6 @@ class Rectangle(BaseGeometry):
     """
     def __init__(self, width, height):
         super().integer_validator("width", width)
-        super().integer_validator("heigth", height)
+        super().integer_validator("height", height)
         self.__width = width
         self.__height = height
