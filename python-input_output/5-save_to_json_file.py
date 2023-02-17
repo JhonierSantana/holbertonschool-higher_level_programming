80c2abeda2ddbca1303d0f3f55f2b17765022487
root <root@DESKTOP-ISQ43BP>
2023-02-17 11:37:16 -0500

    5-save_to_json_file.py

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    Module to write from an object to a file
+"""
+
+
+def save_to_json_file(my_obj, filename):
+    """
+        save_to_json_file - writes my_obj to file filename
+    """
+    import json
+
+    with open(filename, encoding='utf-8', mode='w') as file:
+        file.write(json.dumps(my_obj))
