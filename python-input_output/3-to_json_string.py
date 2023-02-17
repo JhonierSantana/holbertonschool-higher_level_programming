4748f800dd19d5559d3df4eea3914924df1d2b3c
root <root@DESKTOP-ISQ43BP>
2023-02-17 11:22:26 -0500

    3-to_json_string.py

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/python3
+"""
+    Module with JSON representation.
+"""
+
+
+def to_json_string(my_obj):
+    """
+        returns the JSON representation of an object (string)
+    """
+    import json
+    
+    return json.dumps(my_obj)
