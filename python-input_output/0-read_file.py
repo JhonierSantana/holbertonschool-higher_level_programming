766206c281ad9cc3a69e43ed393c28b01fe9935e
root <root@DESKTOP-ISQ43BP>
2023-02-17 10:16:55 -0500

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+    Module with a read function
+"""
+
 
 def read_file(filename=""):
+    """
+        read_file: Reads a text file using UTF8 encoding
+    """
     with open(filename, encoding='utf-8') as file:
         print(file.read(), end="")
