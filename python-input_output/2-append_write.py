7376074410d3c0c716e5e9c9877df8d6f5e35fd0
root <root@DESKTOP-ISQ43BP>
2023-02-17 10:43:23 -0500

    2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -6,8 +6,8 @@
 
 def write_file(filename="", text=""):
     """
-        write_file - writes some text on a file. This overwrites the
-        content of the file
+        write_file - writes some text on a file. This doesn't overwrite the
+        content of the file.
     """
     with open(filename, encoding='utf-8', mode='a') as file:
         return(file.write(text))
