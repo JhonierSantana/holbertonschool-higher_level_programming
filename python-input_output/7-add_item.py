cef75a06db52721a26ab856e8e4521c2bf1c06fe
root <root@DESKTOP-ISQ43BP>
2023-02-17 12:05:22 -0500

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,24 +1,12 @@
 #!/usr/bin/python3
+"""save_to_json_file
 """
-    Script to save arguments to a file
-"""
-
-import sys
 import json
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+def save_to_json_file(my_obj, filename):
+    """Writes an object to a text file, using JSON
+    """
 
-try:
-    arguments = load_from_json_file("add_item.json")
-except Exception:
-    arguments = []
-
-i = 1
-while i < len(sys.argv):
-    arguments.append(sys.argv[i])
-    i += 1
-
-with open("add_item.json", encoding='utf-8', mode='w') as file:
-    file.write(json.dumps(arguments))
+    with open(filename, mode="w", encoding="UTF-8") as saveFile:
+        json.dump(my_obj, saveFile)
