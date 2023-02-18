50787aa195298843c7d70bc0993eee0828646b83
root <root@DESKTOP-ISQ43BP>
2023-02-17 19:05:16 -0500

    upgrade

---
+++ python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    Module with student class
+"""
+
+
+class Student():
+    """
+        Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs and all(type(attr) is str for attr in attrs) \
+                or (type(attrs) is list and len(attrs) == 0):
+            ret_dict = dict()
+            for (key, value) in (self.__dict__).items():
+                for attr in attrs:
+                    if key == attr:
+                        ret_dict[key] = value
+            return (ret_dict)
+        return (self.__dict__)
+
+    def reload_from_json(self, json):
+        if (json and len(json) > 0):
+            first_name = json["first_name"]
+            last_name = json["last_name"]
+            age = json["age"]
+            self.__init__(first_name, last_name, age)
