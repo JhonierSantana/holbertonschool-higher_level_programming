ca5364e71d0389cd336a384b5c40cba6c3cbe885
root <root@DESKTOP-ISQ43BP>
2023-02-22 18:12:43 -0500

    upgrade

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+""" First class Base """
 import json
+from os import path
+
+
 class Base:
+    """ Base """
+    
     __nb__objects = 0
-    
-    
+
     def __init__(self, id=None):
+        """ constructor initialization """
         if id is not None:
             self.id = id
         
