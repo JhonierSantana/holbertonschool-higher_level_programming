0995a412bff7c69b58009dd3a737954041b4d04b
root <root@DESKTOP-ISQ43BP>
2023-02-23 10:53:35 -0500

    upgrade

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+""" Module Class Square """
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """ Class Rectangle """
     def __init__(self, size, x=0, y=0, id=None):
         self.size = size
         super().__init__(size, size, x, y, id)
