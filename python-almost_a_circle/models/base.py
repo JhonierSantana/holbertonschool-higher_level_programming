18ccccb8d3125328bcb4d3308937f0bf46aa2cb7
root <root@DESKTOP-ISQ43BP>
2023-02-23 18:15:26 -0500

    upgrade

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -27,3 +27,41 @@ class Base:
                 list.append(element)
         return(json.dumps(list))
 
+    @classmethod
+    def save_to_file(cls, list_objs):
+        list2 = []
+        with open("{}.json".format(cls.__name__), "w") as file:
+            if (list_objs):
+                for obj in list_objs:
+                    obj_dict = obj.to_dictionary()
+                    list2.append(obj_dict)
+            file.write(cls.to_json_string(list2))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if (json_string):
+            return(json.loads(json_string))
+        return([])
+    
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == "Rectangle":
+            new = cls(1, 1)
+        elif cls.__name__ == "Square":
+            new = cls(1)
+        new.update(**dictionary)
+        return (new)
+
+    @classmethod
+    def load_from_file(cls):
+        ret = []
+        try:
+            with open("{}.json".format(cls.__name__), 'r') as file:
+                str_list = file.read()
+            dict_list = cls.from_json_string(str_list)
+        except:
+            dict_list = dict()
+        for obj_dict in dict_list:
+            new_obj = cls.create(**obj_dict)
+            ret.append(new_obj)
+        return(ret)
