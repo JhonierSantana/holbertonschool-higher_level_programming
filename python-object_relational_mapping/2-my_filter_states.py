aec2a624b15b13899831c2c8fa0c2508a142a587
root <root@DESKTOP-ISQ43BP>
2023-03-17 10:11:15 -0500

    2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    matchname = sys.argv[4]
+    matchName = sys.argv[4]
 
     conn = MySQLdb.connect(host="localhost", port=3306, user=username,
                            passwd=password, db=database, charset="utf8")
