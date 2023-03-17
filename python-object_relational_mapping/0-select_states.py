55f8eac0a53c52bf9644fd14090bf027a609e5e6
root <root@DESKTOP-ISQ43BP>
2023-03-17 09:45:42 -0500

    0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,17 +2,20 @@
 """
     Module to list all states
 """
-
+import MySQLdb
+import sys
+    
 if __name__ == "__main__":
-    import sys
-    import MySQLdb
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
     
-    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
-                         passw=sys.argv[2], db=sys.argv[3])
-    
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    conn = MySQLdb.connect(host="localhost" port=3306, user=username, passwd=password, db=database)
+    cur = conn.cursor()
+    cur.execute("SELEC * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
     cur.close()
+    conn.close()
+ 
