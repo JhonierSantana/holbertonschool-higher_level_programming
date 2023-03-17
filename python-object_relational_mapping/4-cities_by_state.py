0b7c24387cffc52012599b09c8a2f5199e7ac637
root <root@DESKTOP-ISQ43BP>
2023-03-17 11:00:43 -0500

    4-cities_by_state.py

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""forking
+"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=database, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELEC cities.id, cities.name, states.name\
+        FROM cities JOIN states ON cities.states_id = states.id")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
