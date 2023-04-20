aaf2aa8de6ae5b01768bd63de46cb3b9ab7e94fb
root <root@DESKTOP-ISQ43BP>
2023-04-20 11:03:09 -0500

    upgrade

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -2,22 +2,23 @@
 
 const request = require('request');
 const { argv } = require('process');
-const completedTask = {};
+const completedTasks = {};
 
-request(argv[2], (error, response, body) => {
+request(argv[2], function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
     const obt = JSON.parse(body);
-    for (const ent of obt) {
-      if (ent.completed) {
-        const userId = ent.userId;
-        if (Object.keys(completedTask).includes(String(userId))) {
-          completedTask[userId] += 1;
+    for (const entry of obt) {
+      if (entry.completed) {
+        const userId = entry.userId;
+        if (Object.keys(completedTasks).includes(String(userId))) {
+          completedTasks[userId] += 1;
         } else {
-          completedTask[userId] = 1;
+          completedTasks[userId] = 1;
         }
       }
     }
+    console.log(completedTasks);
   }
 });
